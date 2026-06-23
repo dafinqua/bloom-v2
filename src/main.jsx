e5741f07d415ed3295bba6a190ccd113ae8b2911
dafinqua <dafinqua1@gmail.com>
2026-06-23 13:42:48 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('Service Worker registered'))
+      .catch((error) => console.log('Service Worker registration failed:', error));
+  });
+}

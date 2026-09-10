e45861630e9e683bd39623af8137aec3405d72c9
dafinqua <dafinqua1@gmail.com>
2026-09-10 19:33:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5304,12 +5304,7 @@ function AdminDashboard() {
   const [stats, setStats] = useState(null);
   const [screens, setScreens] = useState([]);
   const [error, setError] = useState(false);
-  useEffect(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    console.log("ADMIN SESSION:", data.session);
-    console.log("ADMIN EMAIL:", data.session?.user?.email);
-  });
-}, []);
+
 
   useEffect(() => {
     async function loadDashboard() {
